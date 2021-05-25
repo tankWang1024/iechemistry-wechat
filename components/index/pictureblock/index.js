fdb44ee681896dd8d71793cf8fb10bf27ef6f1cd
张宇弛 <2669327287@qq.com>
2021-05-25 23:47:32 +0800

    add details & analyse page

---
+++ components/index/pictureblock/index.js
@@ -0,0 +1,24 @@
+// components/index/pictureblock/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    value1:"255,255,255",
+    value2:"125,125,125"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
